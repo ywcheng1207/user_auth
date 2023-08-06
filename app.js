ba234faf8ad9f510fb4e2db480feab8d9f2f1e64
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-08-07 04:42:52 +0800

    feat: add cookie-parser

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const Account = require('./models/account')
 const account = require('./models/account')
+const cookieParser = require('cookie-parser')
 require('./config/mongoose')
 
 //
@@ -11,10 +12,16 @@ const port = process.env.PORT || 3000
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 //
 app.get('/', (req, res) => {
-  res.render('index')
+  const userId = req.cookies.userId
+  if (userId) {
+    res.redirect(`/welcome/${userId}`)
+  } else {
+    res.render('index')
+  }
 })
 
 app.post('/', (req, res) => {
@@ -28,6 +35,7 @@ app.post('/', (req, res) => {
         res.render('index', { message: '帳號不存在！' })
       }
       if (account && account.password === password) {
+        res.cookie('userId', account._id, { maxAge: 3600000 })
         res.redirect(`/welcome/${account._id}`)
       } else {
         res.render('index', { message: '密碼錯誤！' })
